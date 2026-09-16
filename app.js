091247928ad8137a56522647e2f362617030e478
siaps-varjota <previnevarjota@gmail.com>
2026-09-16 16:02:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -672,7 +672,6 @@
     "HANNA LUIZA OLIVEIRA GOMES",
     "KARISE SANTOS VASCONCELOS",
     "ANNA MAEVILLY LIRA LOPES MARTINS"
-    "LETÍCIA EMILLY MESQUITA DE SOUSA"
   ].map(normalizeText);
   function ehProfissionalComparativoEmulti(nome){
     return PROFISSIONAIS_COMPARATIVO_EMULTI.indexOf(normalizeText(nome)) >= 0;
